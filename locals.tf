c57bcda809c85432e241878b153d1ccde529c86e
Nick Hammond <n.hammond@waracle.com>
2021-12-14 22:22:50 +0000

    build: Updated release process
    Signed-off-by: Nick Hammond <n.hammond@waracle.com>

--- locals.tf
+++
@@ -1,1 +0,0 @@
-## "Terraform Module Version" = ".*"
--- release.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  branches: ['main'],
-  extends: [
-    '@jortfal/semantic-release-terraform-config'
-  ],
-}
-
-module.exports = config;
