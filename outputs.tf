82b4b379211193f09e51e3063e307b993633fb9a
Nick Hammond <n.hammond@waracle.com>
2021-12-31 09:54:50 +0000

    docs: Add lumigo role ARN description
    Signed-off-by: Nick Hammond <n.hammond@waracle.com>

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 # The ARN of the role that can be assumed by Lumigo's account.
 output "lumigo_role_arn" {
+  description = "The ARN of the generated Lumigo role"
   value = aws_iam_role.lumigo_integration_role.arn
 }
