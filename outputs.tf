7eaae6cf45bd00ba569a6f4467a45912a0667379
Nick Hammond <n.hammond@waracle.com>
2021-12-31 09:55:10 +0000

    refactor: Formatting
    Signed-off-by: Nick Hammond <n.hammond@waracle.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # The ARN of the role that can be assumed by Lumigo's account.
 output "lumigo_role_arn" {
   description = "The ARN of the generated Lumigo role"
-  value = aws_iam_role.lumigo_integration_role.arn
+  value       = aws_iam_role.lumigo_integration_role.arn
 }
